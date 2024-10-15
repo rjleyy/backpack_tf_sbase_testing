f83a7d45c38724327a708c21359f589fab91eb6e
rjleyy <rileylarsen19@gmail.com>
2024-10-15 10:55:19 -0400

    Lowering sleep time and number of browsers running on browserstack

--- specs/test_mainpage.py
+++ specs/test_mainpage.py
@@ -10,7 +10,7 @@ class TestMainPage(BaseTest):
         self.mainpage = MainPage(self)
         self.mainpage.open_backpack_tf()
         # Adding sleep to bypass human verification trigger
-        self.sleep(45)
+        self.sleep(35)
 
     def tearDown(self):
         print("Log Out")
